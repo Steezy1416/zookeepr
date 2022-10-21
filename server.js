e86306b3fbc8bfc6c24700688b92f2c39aed0541
alexfortin <fortin.alex14@gmail.com>
2022-10-21 17:59:47 -0500

    handle request for specific animal

--- server.js
+++ server.js
@@ -33,8 +33,6 @@ function filterByQuery(query, animalsArray) {
         )
     })
 
-
-
     if (query.diet) {
         filteredResults = filteredResults.filter(animal => animal.diet === query.diet)
     }
@@ -47,6 +45,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults
 }
 
+function findByID(id, animalsArray){
+    const result = animalsArray.filter(animal => animal.id === id)[0]
+    return result
+}
+
 app.get("/api/animals", (req, res) => {
     let results = animals
     if (req.query) {
@@ -55,6 +58,17 @@ app.get("/api/animals", (req, res) => {
     res.json(results)
 })
 
+app.get("/api/animals/:id", (req, res) => {
+    const result = findByID(req.params.id, animals)
+    
+    if(result) {
+        res.json(result)
+    }
+    else {
+        res.send(404)
+    }
+})
+
 app.listen(PORT, () => {
-    console.log("API server now on port 3001!")
+    console.log(`API server now on port ${PORT}!`)
 })
